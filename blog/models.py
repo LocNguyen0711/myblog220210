6fb65a11034eca3e3238bc84970dbb3fd21ec4e5
Loc Nguyen <ntloc2001195@student.ctuet.edu.vn>
2022-02-14 21:03:33 +0700

    modify

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
+from time import timezone
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class mCreatepost(models.Model):
      title = models.CharField(max_length=200)
      discription = models.TextField()
-     body = models.TextField()
+     body = models.TextField()     
      def __str__(self):
           return f"{self.id}: {self.title} - {self.discription}"
-     
